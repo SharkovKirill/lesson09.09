f945c2a2925234fb3300b63f8f8e523cbcfbfcb2
SharkovKirill <55428474+SharkovKirill@users.noreply.github.com>
2020-09-09 09:46:51 +0700

    Update para.py

--- para.py
+++ para.py
@@ -5,9 +5,8 @@ def decorator(func):
     @functools.wraps(func)
     def wrapped(*args):
         list = [*func(*args)]
-        with open('list.yaml', 'w') as f:
-            yaml.dump(list)
-            print(yaml.dump(list))
-        return list
+        #with open('list.yaml', 'w') as f:
+            #yaml.dump(list)
+            #print(yaml.dump(list))
+        return yaml.dump(list)
     return wrapped
-
